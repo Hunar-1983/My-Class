202fbff5f8f27ab0b84d3285d62fb25332ee5610
Hunar-1983 <parezarhunar@gmail.com>
2020-04-29 17:18:49 -0400

    add

--- src/My_Practice/wewe.java
+++ src/My_Practice/wewe.java
@@ -1,6 +1,25 @@
 package src.My_Practice;
 
-public class wewe {
+import src.Important.Array;
 
+import java.util.Arrays;
+
+public class wewe {
+    public static String[] reverse(String[] arr) {
+        String [] arr1 = new String[arr.length];
+        int k = arr.length-1;
+
+        for (int i = 0; i < arr.length; i++){
+            arr1[k] = arr[i];
+            k--;
+        }
+        return arr1;
+    }
+    public static void main(String[] args) {
+    String [] arr = {"apple", "pear"};
+
+    String [] str = reverse(arr);
+        System.out.println(Arrays.toString(str));
 
     }
+}
---
+++ src/OfficeHours/Practice_04_29_2020/ArrayList_Methods2.java
@@ -0,0 +1,8 @@
+package src.OfficeHours.Practice_04_29_2020;
+
+public class ArrayList_Methods2 {
+    public static void main(String[] args) {
+
+
+    }
+}
